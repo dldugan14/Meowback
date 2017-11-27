58eb7f38cc91586c380f52e4d5076547ea68c163
Dillon Dugan <dugan_dillon@yahoo.com>
2017-11-27 13:51:23 -0700

    nice part 02

--- app.js
+++ app.js
@@ -4,11 +4,20 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var mongoose = require("mongoose");
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+require("dotenv").config();
+
 var app = express();
+
+const db = mongoose.connection;
+mongoose.connect(process.env.MONGO_URI);
+db.on("error", console.error.bind(console, "CONNECTION FAILIURE, GO HOME:"));
+db.once("open", function() {
+  console.log("Your db is good to go!!!!!");
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
